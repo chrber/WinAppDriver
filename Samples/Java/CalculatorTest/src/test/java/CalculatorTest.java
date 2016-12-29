97174097c7e37410653e941c84e6bddae63fd6aa
Christian Bernardt <christian.bernardt@desy.de>
2016-12-29 12:02:31 +0100

    Adapt calculator test to windows machine address that appdriver is runing on

--- Samples/Java/CalculatorTest/src/test/java/CalculatorTest.java
+++ Samples/Java/CalculatorTest/src/test/java/CalculatorTest.java
@@ -14,12 +14,13 @@
 //
 //******************************************************************************
 
+import io.appium.java_client.ios.IOSDriver;
 import org.junit.*;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import java.util.concurrent.TimeUnit;
+
 import java.net.URL;
-import io.appium.java_client.ios.IOSDriver;
+import java.util.concurrent.TimeUnit;
 
 public class CalculatorTest {
 
@@ -31,7 +32,7 @@ public class CalculatorTest {
         try {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
-            CalculatorSession = new IOSDriver(new URL("http://127.0.0.1:4723"), capabilities);
+            CalculatorSession = new IOSDriver(new URL("http://131.169.137.143:4723"), capabilities);
             CalculatorSession.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
             CalculatorSession.findElementByName("Clear").click();
